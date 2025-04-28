408c414c982c7e637e8e902f7c9a786ef0e1315f
Estefanía Q. Lusby <estefaniaquevedolusby@gmail.com>
2025-04-28 11:12:37 +0200

    exercise 1.7 expanding statistics

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -6,6 +6,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const total = good + bad + neutral
+  const average = total === 0 ? 0 : (good - bad) /total
+  const positivePercentage = total === 0 ? 0 : (good/total) * 100
+
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -13,10 +17,14 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad+1)}>bad</button>
 
+  
       <h1>Statistics</h1>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {total}</p>
+      <p>Average: {average.toFixed(1)}</p>
+      <p>Positive: {positivePercentage.toFixed(1)} %</p>
     </div>
   );
 };
