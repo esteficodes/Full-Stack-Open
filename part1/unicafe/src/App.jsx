445cf2f49173508e2c3e032d2c84e2bb4c9731d0
Estefanía Q. Lusby <estefaniaquevedolusby@gmail.com>
2025-04-29 10:52:24 +0200

    erased and started again as hadn't installed vite. Created exercise 1.10

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const StatisticLine = ({ text, value }) => {
+  <p>{text}: {value}</p>
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+
+  if (total === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
+  const average = (good - bad) / total;
+  const positivePercentage = (good / total) * 100;
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <StatisticLine text="Good" value={good}/>
+      <StatisticLine text="Neutral" value={neutral}/>
+      <StatisticLine text="Bad" value={bad}/>
+      <StatisticLine text="All" value={total}/>
+      <StatisticLine text="Average" value={average.toFixed(1)}/>
+      <StatisticLine text="Positive" value={`${positivePercentage.toFixed(1)} %`}/>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
